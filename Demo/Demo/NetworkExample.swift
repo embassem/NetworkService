8151708915f23512df5659331c1f3eee3b76cfa7
Bassem Abbas <em.bassem@gmail.com>
2017-08-30 22:19:37 +0200

    **Task** # :
    Work Done :
    ==========
    -  add example
    
    Remaining work :
    =============
    -
    ``

--- Demo/Demo/NetworkExample.swift
+++ Demo/Demo/NetworkExample.swift
@@ -32,20 +32,103 @@ class GithubProvider: NSObject  , ProviderProtocal {
     
     func getprovider() -> MoyaProvider<Target>  {
         
-        let endpointClosure = { (target: Target) -> Endpoint<GitHub> in
+        let endpointClosure = { (target: Target) -> Endpoint<Target> in
             let defaultEndpoint = MoyaProvider.defaultEndpointMapping(for: target)
-                .adding(newHTTPHeaderFields: ["X-Api-Key": "ABCD"])
-                .adding(newHTTPHeaderFields: ["X-Api-Language": "AR"]);
+//                .adding(newHTTPHeaderFields: ["X-Api-Key": "ABCD"])
+//                .adding(newHTTPHeaderFields: ["X-Api-Language": "AR"]);
             
             return defaultEndpoint
         }
         let token = ""
         let moyaProvider =  MoyaProvider<Target>(endpointClosure: endpointClosure,
-                                                 plugins: [ NetworkLoggerPlugin(verbose: true, responseDataFormatter: JSONResponseDataFormatter),
-                                                            AccessTokenPlugin(token: token )])
+                                                 plugins: [ NetworkLoggerPlugin(verbose: true, responseDataFormatter: JSONResponseDataFormatter) ])//,
+//                                                            AccessTokenPlugin(token: token )])
         
         return moyaProvider
     }
     
     
 }
+
+
+class ibtikarProvider: NSObject  , ProviderProtocal {
+    
+    
+    typealias Target = IbtikarAPI
+    //    typealias ProviderTargetType = MoyaProvider<APITarget>
+    var providerBaseUrl:String
+    
+    required init(baseUrl: String, plugins: [PluginType] , headers:[[String: String]]) {
+        self.providerBaseUrl = baseUrl;
+    }
+    
+    
+    
+    func changeBaseUrl() {
+        
+        
+    }
+    
+    func getprovider() -> MoyaProvider<Target>  {
+        
+        let endpointClosure = { (target: Target) -> Endpoint<Target> in
+            let defaultEndpoint = MoyaProvider.defaultEndpointMapping(for: target)
+                            .adding(newHTTPHeaderFields: ["X-Api-Key": "BNZrYUfnqkBNZOhx2u1wonkBrYpnqkBNZrYOhxl"])
+            //                .adding(newHTTPHeaderFields: ["X-Api-Language": "AR"]);
+            
+            return defaultEndpoint
+        }
+      
+        let moyaProvider =  MoyaProvider<Target>(endpointClosure: endpointClosure,
+                                                 plugins: [ NetworkLoggerPlugin(verbose: true, responseDataFormatter: JSONResponseDataFormatter) ])//,
+        //                                                            AccessTokenPlugin(token: token )])
+        
+        return moyaProvider
+    }
+    
+    
+}
+
+class MOIAProvider: NSObject  , ProviderProtocal {
+    
+    
+    typealias Target = MoiaAPI
+    //    typealias ProviderTargetType = MoyaProvider<APITarget>
+    var providerBaseUrl:String
+    
+    required init(baseUrl: String, plugins: [PluginType] , headers:[[String: String]]) {
+        self.providerBaseUrl = baseUrl;
+    }
+    
+    
+    
+    func changeBaseUrl() {
+        
+        
+    }
+    
+    func getprovider() -> MoyaProvider<Target>  {
+        
+        let endpointClosure = { (target: Target) -> Endpoint<Target> in
+            let defaultEndpoint = MoyaProvider.defaultEndpointMapping(for: target)
+            //                .adding(newHTTPHeaderFields: ["X-Api-Key": "ABCD"])
+            //                .adding(newHTTPHeaderFields: ["X-Api-Language": "AR"]);
+            
+            return defaultEndpoint
+        }
+        
+        let moyaProvider =  MoyaProvider<Target>(endpointClosure: endpointClosure,
+                                                 plugins: [ NetworkLoggerPlugin(verbose: true, responseDataFormatter: JSONResponseDataFormatter) ])//,
+        //                                                            AccessTokenPlugin(token: token )])
+        
+        return moyaProvider
+    }
+    
+    
+}
+
+
+
+
+
+
--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -15,17 +15,18 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         
+//         IbtikarProvider(baseUrl: "https://api.github.com", plugins: [], headers: []).getprovider()
+    NetworkService.config(providers: [ MOIAProvider(baseUrl: "https://api.github.com", plugins: [], headers: []).getprovider(),ibtikarProvider(baseUrl: "https://api.github.com", plugins: [], headers: []).getprovider()])
         
-    NetworkService.config(providers: [GithubProvider(baseUrl: "https://api.github.com", plugins: [], headers: []).getprovider()])
         
         
-        NetworkService.shared.request(endPoint: GitHub.userRepositories("ashfurrow"), modelType: userRepo.self ,responseType: .array) { (result, json, response, type) in
+        NetworkService.shared.request(endPoint:IbtikarAPI.aboutProgram, modelType: AboutProgram.self ,responseType: .object) { (result, json, response, type) in
             
             switch result {
                 
             case .success(let res)  :
                 
-                if let userprofile  = res as? GenaricModel<GithubRoot> {
+                if let userprofile  = res as? GenaricModel<AboutProgram> {
                     
                     
                 }else {
@@ -42,6 +43,31 @@ class ViewController: UIViewController {
                 
             }
         }
+        
+//        
+//        NetworkService.shared.request(endPoint: GitHub.userRepositories("ashfurrow"), modelType: userRepo.self ,responseType: .array) { (result, json, response, type) in
+//            
+//            switch result {
+//                
+//            case .success(let res)  :
+//                
+//                if let userprofile  = res as? GenaricModel<userRepo> {
+//                    
+//                    
+//                }else {
+//                    
+//                }
+//                
+//                break ;
+//                
+//                
+//                
+//            case .failure(let error ) :
+//                
+//                return
+//                
+//            }
+//        }
         
     }
 
